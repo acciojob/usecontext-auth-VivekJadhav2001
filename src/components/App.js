0ecf121dda39169cc905ad553e8723dddcbce1e9
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-28 02:57:33 +0530

    rendering UseContextAuth inside App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import UseContextAuth from "./UseContextAuth";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <UseContextAuth/>
     </div>
   )
 }
