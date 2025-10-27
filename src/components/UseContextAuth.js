f5a6fdc72ebcec2121d70c642045d75228118eba
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-28 03:04:47 +0530

    text correction -2

--- src/components/UseContextAuth.js
+++ src/components/UseContextAuth.js
@@ -9,7 +9,7 @@ function UseContextAuth() {
       <h1>Click on the checkbox to get authenticated</h1>
 
       {isAuth ? (
-        <p className="authText">you are now authenticated</p>
+        <p className="authText">You are now authenticated, you can proceed</p>
       ) : (
         <p>you are not authenticated</p>
       )}
